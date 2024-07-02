de315e30d13372f3e420cd9429ebaabcabc8effc
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-07-02 21:26:16 +0530

    Division by Zero test added

--- tests/test_division.py
+++ tests/test_division.py
@@ -29,3 +29,31 @@ class TestDivision:
         delta = 0.0001  # Adjust delta as per your tolerance level
         assert abs(final_result_float - operation_result) < delta
 
+
+    @allure.title("Checking division operation with zero")
+    def test_division_by_zero_operation(self, appium_driver):
+        wait = WebDriverWait(appium_driver, 10)
+
+        first_number: int = 5
+        second_number: int = 0
+
+        print(f'Checking the division of {first_number} and {second_number}')
+        
+        number_five = wait.until(EC.element_to_be_clickable(Locators.number_five))
+        number_zero = wait.until(EC.element_to_be_clickable(Locators.number_zero))
+        division_operation = wait.until(EC.element_to_be_clickable(Locators.divide_operation)) 
+
+        number_five.click()
+        division_operation.click()
+        number_zero.click()
+
+        equal_button = wait.until(EC.element_to_be_clickable(Locators.equal_button))
+        equal_button.click()
+
+        result_preview = wait.until(EC.presence_of_element_located(Locators.result_preview))
+        result_preview_text = result_preview.text
+        is_displayed = result_preview.get_attribute("displayed")
+        print(f'Warning is displayed ? {is_displayed}')
+
+        assert result_preview_text == "Can't divide by 0"
+        assert is_displayed == "true"
